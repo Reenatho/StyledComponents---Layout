b0b2a75e4a0b433e03c68676a10a268c64855dba
Reenatho <r-e-n-a-t-h-o@hotmail.com>
2022-01-19 10:58:45 -0300

    Estilização de paginas usando o StyledComponentes e efeito visual DarkTheme

---
+++ precache-manifest.5492a8896c7a391330a5976b4fc8d484.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ffee2281b0025e0048d1c329c578bb4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e5e587f477695db840a5",
+    "url": "./static/js/2.7323cafa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.7323cafa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29ff21a8db8d7a2f194a",
+    "url": "./static/js/main.d0c328c6.chunk.js"
+  },
+  {
+    "revision": "468723f787d90fa2ffa4",
+    "url": "./static/js/runtime-main.ae645b12.js"
+  },
+  {
+    "revision": "44dbaa78fb2ed240792c9e7a424b0ea0",
+    "url": "./static/media/alimentacao.44dbaa78.svg"
+  },
+  {
+    "revision": "f3a2bff49642ca68b3841fde422295cb",
+    "url": "./static/media/bank_logo.f3a2bff4.svg"
+  },
+  {
+    "revision": "b97c300bed11302a8687c16e07aa89e5",
+    "url": "./static/media/dinheiro.b97c300b.svg"
+  },
+  {
+    "revision": "acd41ceb3366a71c61b7ed3d5df83682",
+    "url": "./static/media/olho.acd41ceb.svg"
+  },
+  {
+    "revision": "17e95f3cc45dc23ffda7f4d29dc137df",
+    "url": "./static/media/outros.17e95f3c.svg"
+  },
+  {
+    "revision": "134d853a95b2b30676da2c63cda2eeaf",
+    "url": "./static/media/privado.134d853a.svg"
+  },
+  {
+    "revision": "605ef9ca7e70a1bb568ffcec443d05f6",
+    "url": "./static/media/saude.605ef9ca.svg"
+  },
+  {
+    "revision": "0981f5bfd5f197c52e678c3c0616cd81",
+    "url": "./static/media/themeOff.0981f5bf.svg"
+  },
+  {
+    "revision": "75ea28146734a5ea6593dab45d272a90",
+    "url": "./static/media/themeOn.75ea2814.svg"
+  },
+  {
+    "revision": "15a4ed7310630a72e21a8054897c3a51",
+    "url": "./static/media/transporte.15a4ed73.svg"
+  },
+  {
+    "revision": "59035811601fe0a607827eb6408e14ef",
+    "url": "./static/media/utilidades.59035811.svg"
+  }
+]);
